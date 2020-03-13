4a672b6dd7843a71df68b37cad1656b9c253315c
Ryan Marten <ryanmarten@Ryans-MacBook-Pro-2.local>
2020-03-12 23:07:45 -0400

    create starter files for orderdistribution

---
+++ Classes/OrderDistribution.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+
+class OrderDistribution(ABC):
+	"""
+	An OrderDistribution class
+
+	Abstract class that defines the interface of a order delivery service
+	"""
+
+	def __init__(self):
+		pass
+
+	@abstractmethod
+	def set_address(self):
+		pass
+
+	def get_address(self):
+		pass
+
+
+	def __str__(self):
+		"""
+		Return to later once we know what we want out of this
+		:return:
+		"""
+		pass
